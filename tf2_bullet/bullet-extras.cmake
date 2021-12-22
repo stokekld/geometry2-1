8d9c60afa310f6d8729f38105a6f12bf93a2edea
Shane Loretz <sloretz@osrfoundation.org>
2021-12-22 12:24:21 -0800

    Install includes to include/${PROJECT_NAME} and use modern CMake
    Signed-off-by: Shane Loretz <sloretz@osrfoundation.org>

--- tf2_bullet/bullet-extras.cmake
+++ tf2_bullet/bullet-extras.cmake
@@ -26,9 +26,13 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
+# Bullet CMake is using old style variables; this extra script makes a target for it
+
 if(WIN32)
   set(BULLET_ROOT $ENV{ChocolateyInstall}/lib/bullet)
 endif()
 find_package(Bullet REQUIRED)
 
-include_directories(SYSTEM ${BULLET_INCLUDE_DIRS})
+add_library(tf2_bullet::Bullet INTERFACE IMPORTED)
+target_include_directories(tf2_bullet::Bullet INTERFACE ${BULLET_INCLUDE_DIRS})
+target_link_libraries(tf2_bullet::Bullet INTERFACE ${BULLET_LIBRARIES})
